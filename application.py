878be21959b382f4a4740f6896903b1d42a596f5
bgriessbach <b.griessbach@gmail.com>
2017-11-18 17:27:15 +0000

    hangman Animation

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ print('''
                         |___/                           
 
 	''')
-print("Welcome to Hangman using Webster Dictionary:\n\n")
+print("Welcome to Hangman using Webster's Dictionary:\n\n")
 wordLength=0
 while wordLength<4 or wordLength>45:
 	userInput=input("How many letters?(4-45)")
@@ -45,22 +45,51 @@ display=g.initialiseDisplay(wordTarget)
 while g.gameEnd(display, wrongLetters)!=True:
 	letter=g.letterInput()
 	if letter in display:
-		print("Letter already found. Hangman grows.")
+		print("Letter already used.")
 		wrongLetters+=1
+		g.animationHangman(wrongLetters)
 	elif letter not in wordTarget:
-		print("Letter not found. Hangman grows.")
+		print("Letter not found.")
 		wrongLetters+=1
+		g.animationHangman(wrongLetters)
 	else:
 		print("Letter found.")
 		display=g.updateDisplay(display, letter, letterIndex)
-	#g.displayHangman(wrongLetters)
 
 
 #End Game either in Loss or Success
 if wrongLetters>10:
-	print("You lost the game.")
+	print('''
+
+ __     __           _           _     _   _                                        
+ \ \   / /          | |         | |   | | | |                                       
+  \ \_/ ___  _   _  | | ___  ___| |_  | |_| |__   ___    __ _  __ _ _ __ ___   ___  
+   \   / _ \| | | | | |/ _ \/ __| __| | __| '_ \ / _ \  / _` |/ _` | '_ ` _ \ / _ \ 
+    | | (_) | |_| | | | (_) \__ | |_  | |_| | | |  __/ | (_| | (_| | | | | | |  __/ 
+    |_|\___/ \__,_| |_|\___/|___/\__|  \__|_| |_|\___|  \__, |\__,_|_| |_| |_|\___| 
+                                                         __/ |                      
+                                                        |___/                       
+                                                                                    
+
+
+
+		''')
 else:
-	print("Congratulations.\n{} : {}".format(wordTarget, wordsWebster[wordTarget]))
+	print('''
+
+
+   _____                            _         _       _   _                  
+  / ____|                          | |       | |     | | (_)                 
+ | |     ___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_ _  ___  _ __  ___  
+ | |    / _ \| '_ \ / _` | '__/ _` | __| | | | |/ _` | __| |/ _ \| '_ \/ __| 
+ | |___| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \__ \ 
+  \_____\___/|_| |_|\__, |_|  \__,_|\__|\__,_|_|\__,_|\__|_|\___/|_| |_|___/ 
+                     __/ |                                                   
+                    |___/                                                    
+                                                                             
+                                                                             
+	''')
+	print("{} : {}".format(wordTarget, wordsWebster[wordTarget]))
 
 
 
